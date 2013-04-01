6724736eb61bbaed5c0a967d643ff32cfb1ad1ec
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-01 17:16:46 +0100

    First version of partial.py - add minimal partial function application support, add crappy test

--- partial.py
+++ partial.py
@@ -2,20 +2,42 @@ from inspect import getargspec
 
 class partial(object):
     """
-    Takes a function, returning a partial object
-    Captures arguments until there's enough to call
+    Takes a function, returning a partial object which 
+    captures arguments until there's enough to call the function.
     """
     def __init__(self, f, *varargs):
-        self.f = f
-        self.maxargs = len(getargspec(self.f)[args])
-        self.posargs = varargs
+        self._f = f
+        self._min_args = len(getargspec(f).args)
+        self._positional_args = list(varargs)
 
-        self._enough_args()
+        self.__call__() # Execute immediately if there's enough arguments.
+
+
+    def __call__(self, *varargs):
+        self._positional_args.extend(list(varargs))
+
+        if self._enough_args():
+            return self._f(*self._positional_args)
+        else:
+            return self
+
 
     def _enough_args(self):
-        if len(posargs) > self.maxargs:
-            pass # Exception goes here
-        elif len(posargs) == self.maxargs:
-            return True
-        else:
+        """
+        Return a boolean indicating if there are enough arguments or not.
+        """
+        num_args = len(self._positional_args)
+
+        if num_args < self._min_args:
             return False
+        else:
+            return True
+            # Author's Note: right now it doesn't deal with there being
+            #                too many arguments. Instead it just lets the
+            #                function fail.
+
+
+def test():
+    f = partial(lambda a,b,c: (a,b,c))
+    g = f('a', 'b')
+    print g('c')
